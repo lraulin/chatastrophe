417d4e250cba6d2f79dcf607d1c6b7a8ca452b54
Lee Raulin <leeraulin@gmail.com>
2018-04-13 21:38:57 -0400

    set up webpack

---
+++ src/index.js
@@ -0,0 +1,3 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+ReactDOM.render(React.createElement('hi', null, 'Hello from React!'), document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  entry: __dirname + "/src/index.js",
+  output: {
+    path: __dirname + "public",
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  devServer: {
+    contentBase: "./public",
+    historyApiFallback: true,
+    inline: true,
+  }
+};
