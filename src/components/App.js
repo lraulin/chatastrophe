9b78c39b6443856a8466dcd69919b5d9388389c9
Lee Raulin <leeraulin@gmail.com>
2018-04-15 19:23:06 -0400

    added routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import LoginContainer from './LoginContainer';
+import ChatContainer from './ChatContainer';
 import './app.css';
 
 export default class App extends Component {
-  greeting = 'Hello from React!!';
-  logGreeting = () => {
-    console.log(this.greeting);
+  state = { user: null };
+
+  componentDidMount() {
+    console.log('Did Mount');
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      }
+    });
   }
 
   render() {
-    this.logGreeting()
     return (
       <div id="container" className="inner-container">
-        <LoginContainer/>
+        <Route path="/login" component={LoginContainer} />
+        <Route exact path="/" component={ChatContainer} />
       </div>
-    )
+    );
   }
 }
--- src/components/LoginContainer.js
+++ src/components/LoginContainer.js
@@ -1,42 +1,88 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 
 class LoginContainer extends Component {
   state = {
     email: '',
-    password: ''
+    password: '',
+    error: '',
   };
 
+  login() {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then((res) => {
+        this.onLogin();
+      })
+      .catch((err) => {
+        if (err.code === 'auth/user-not-found') {
+          this.signup();
+          console.log('Signing up.');
+        } else {
+          this.setState({ error: 'Error logging in.' });
+        }
+      });
+  }
+
+  signup() {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({ error: 'Error signing up.' });
+      });
+  }
+
+  onLogin() {
+    this.props.history.push('/');
+  }
+
   handleEmailChange = (event) => {
-    this.setState({email: event.target.value});
+    this.setState({ email: event.target.value });
   };
 
   handlePasswordChange = (event) => {
-    this.setState({password: event.target.value});
+    this.setState({ password: event.target.value });
   };
 
   handleSubmit = (event) => {
+    console.log('Submit event!');
     event.preventDefault();
-    console.log(this.state);
+    this.setState({ error: '' });
+    if (this.state.email && this.state.password) {
+      this.login();
+    } else {
+      this.setState({ error: 'Please fill in both fields' });
+    }
   };
 
   render() {
     return (
       <div id="LoginContainer" className="inner-container">
-        <Header/>
+        <Header />
         <form onSubmit={this.handleSubmit}>
           <p>Sign in to or sign up by entering your email and password.</p>
           <input
             type="text"
             onChange={this.handleEmailChange}
             value={this.state.email}
-            placeholder="Your Email"/>
+            placeholder="Your Email"
+          />
           <input
             type="password"
             onChange={this.handlePasswordChange}
             value={this.state.password}
-            placeholder="Your Password"/>
-          <button className="red light" type="submit">Login</button>
+            placeholder="Your Password"
+          />
+          <p className="error">{this.state.error}</p>
+          <button className="red light" type="submit">
+            Login
+          </button>
         </form>
       </div>
     );
